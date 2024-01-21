61c9388e21d723df96b9a3a55d02f00fabd86e16
Felipe Blaksley Iraola <felipe.blaksley@hotmail.com>
2024-01-20 23:17:04 -0300

    route orders

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -1,7 +1,31 @@
 import pool from "../db.js";
 
 export const getOrders = async (req, res) => {
-    try {
+  try {
+    const query = `
+        SELECT o.*, p.*, u.* 
+        FROM orders o 
+        JOIN products p ON o.productID = p.id 
+        JOIN users u ON o.userID = u.id;
+      `;
+    const [rows] = await pool.query(query);
+    res.status(202).json(rows);
+  } catch (error) {
+    res.status(404).send("Internal Server Error: " + error.message);
+  }
+};
+
+export const updateOrderStatus = async (req, res) => {
+  const orderId = req.params.id;
+  const newStatus = false;
+
+  try {
+    const updateQuery = "UPDATE orders SET status = ? WHERE id = ?";
+    const [updateResult] = await pool.query(updateQuery, [newStatus, orderId]);
+
+    if (updateResult.affectedRows === 0) {
+      res.status(404).send("Order not found with id " + orderId);
+    } else {
       const query = `
         SELECT o.*, p.*, u.* 
         FROM orders o 
@@ -10,30 +34,11 @@ export const getOrders = async (req, res) => {
       `;
       const [rows] = await pool.query(query);
       res.status(202).json(rows);
-    } catch (error) {
-      res.status(404).send("Internal Server Error: " + error.message);
     }
-  };
-
-  export const updateOrderStatus = async (req, res) => {
-    const orderId = req.params.id;
-    const newStatus = false;
-  
-    try {
-      const updateQuery = "UPDATE orders SET status = ? WHERE id = ?";
-      const [updateResult] = await pool.query(updateQuery, [newStatus, orderId]);
-  
-      if (updateResult.affectedRows === 0) {
-        res.status(404).send("Order not found with id " + orderId);
-      } else {
-        const selectQuery = "SELECT * FROM orders";
-        const [allOrders] = await pool.query(selectQuery);
-        res.status(200).json(allOrders);
-      }
-    } catch (error) {
-      res.status(500).send("Internal Server Error: " + error.message);
-    }
-  };
+  } catch (error) {
+    res.status(500).send("Internal Server Error: " + error.message);
+  }
+};
 
 export const postOrders = async (req, res) => {
   try {
